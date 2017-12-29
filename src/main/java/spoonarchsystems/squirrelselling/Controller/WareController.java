5a3504a40217016f404e3175ce1c0720d3f8acee
StarWitch <dp.starwitch@gmail.com>
2017-12-29 13:39:42 +0100

    - ware controller (mapping /shoppingCart) - shopping cart view (bitten, but not finished) - added href to shopping cart in header (and some minor link adjustments)

---
+++ src/main/java/spoonarchsystems/squirrelselling/Controller/WareController.java
@@ -0,0 +1,13 @@
+package spoonarchsystems.squirrelselling.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WareController {
+
+    @GetMapping("/shoppingCart")
+    public String home() {
+        return "view/general/shopping_cart";
+    }
+}
