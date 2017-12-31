767cab56d13695cc2686e1e1eb566ecaf7933c3d
Tomasz <nowik18@gmail.com>
2017-12-31 19:35:24 +0100

    - added order value computing - updated order details template to display attributes of current order instead of static data

--- src/main/java/spoonarchsystems/squirrelselling/Controller/OrderController.java
+++ src/main/java/spoonarchsystems/squirrelselling/Controller/OrderController.java
@@ -5,8 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import spoonarchsystems.squirrelselling.Model.Service.OrderService;
 import spoonarchsystems.squirrelselling.Model.Entity.Order;
+import spoonarchsystems.squirrelselling.Model.Entity.OrderPosition;
+import spoonarchsystems.squirrelselling.Model.Service.OrderService;
 
 
 @Controller
@@ -18,7 +19,13 @@ public class OrderController {
     @GetMapping("/order/{id}")
     public String showOrderDetails(@PathVariable int id, Model model) {
         Order order = orderService.getOrder(id);
+        double orderValue = 0;
+        for (OrderPosition position : order.getPositions()) {
+            orderValue += position.getPrice() * position.getQuantity();
+        }
+
         model.addAttribute("order", order);
+        model.addAttribute("orderValue", orderValue);
         return "view/order/order_details";
     }
 }
