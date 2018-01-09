ff25f96d5f14004ef07a9c52f1117ca47a17507a
Tomasz <nowik18@gmail.com>
2018-01-10 00:14:31 +0100

    - added function giving order value

--- src/main/java/spoonarchsystems/squirrelselling/Model/Service/OrderService.java
+++ src/main/java/spoonarchsystems/squirrelselling/Model/Service/OrderService.java
@@ -4,4 +4,5 @@ import spoonarchsystems.squirrelselling.Model.Entity.Order;
 
 public interface OrderService {
     Order getOrder(int id);
+    double getOrderValue(Order order);
 }
--- src/main/java/spoonarchsystems/squirrelselling/Model/Service/OrderServiceImpl.java
+++ src/main/java/spoonarchsystems/squirrelselling/Model/Service/OrderServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import spoonarchsystems.squirrelselling.Model.DAO.OrderDAO;
 import spoonarchsystems.squirrelselling.Model.Entity.Order;
+import spoonarchsystems.squirrelselling.Model.Entity.OrderPosition;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -17,5 +18,14 @@ public class OrderServiceImpl implements OrderService {
     public Order getOrder(int id) {
         Order order = orderDAO.getOrder(id);
         return order;
+    }
+
+    @Override
+    public double getOrderValue(Order order) {
+        double orderValue = 0;
+        for (OrderPosition position : order.getPositions()) {
+            orderValue += position.getPrice() * position.getQuantity();
+        }
+        return orderValue;
     }
 }
