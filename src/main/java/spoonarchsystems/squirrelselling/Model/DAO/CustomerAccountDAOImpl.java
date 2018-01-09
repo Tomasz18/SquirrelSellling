ca7f8c7d1c1882800c6c428b3e69f15b7f14c000
Tomasz <nowik18@gmail.com>
2018-01-10 00:19:23 +0100

    - fixed customer account DAO to fetch customer orders

--- src/main/java/spoonarchsystems/squirrelselling/Model/DAO/CustomerAccountDAOImpl.java
+++ src/main/java/spoonarchsystems/squirrelselling/Model/DAO/CustomerAccountDAOImpl.java
@@ -1,11 +1,17 @@
 package spoonarchsystems.squirrelselling.Model.DAO;
 
+import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
 import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import spoonarchsystems.squirrelselling.Model.Entity.CustomerAccount;
+import spoonarchsystems.squirrelselling.Model.Entity.Order;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class CustomerAccountDAOImpl implements CustomerAccountDAO{
@@ -18,6 +24,9 @@ public class CustomerAccountDAOImpl implements CustomerAccountDAO{
     public CustomerAccount getCustomerAccount(int id) {
         Session session = sessionFactory.getCurrentSession();
         CustomerAccount customerAccount = session.load(CustomerAccount.class, new Integer(id));
+        Criteria criteria = session.createCriteria(Order.class).add(Restrictions.eq("customer.id", customerAccount.getCustomer().getId()));
+        ArrayList<Order> orders = (ArrayList<Order>) criteria.list();
+        customerAccount.getCustomer().setOrders(orders);
         return customerAccount;
     }
 }
